839a8f51d55e25caa8ca4d2b74357aa7cf58ed3c
0xCoDSnet <76873593+0xcodsnet@users.noreply.github.com>
2026-01-31 11:23:06 +0400

    v1.2.4

--- modules/common/src/main/java/net/oxcodsnet/beltborne_lanterns/common/client/ui/LanternDebugScreen.java
+++ modules/common/src/main/java/net/oxcodsnet/beltborne_lanterns/common/client/ui/LanternDebugScreen.java
@@ -189,7 +189,7 @@ public class LanternDebugScreen extends Screen {
     }
 
     @Override
-    protected void renderBlurredBackground(float delta) {
+    protected void renderBlurredBackground(GuiGraphics context) {
         // keep world visible
     }
 
